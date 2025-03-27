38d2366ae4249f770d9415a12d938388f41d63c8
Noar2000 <6403011011@feu.edu>
2025-03-27 14:00:33 +0700

    1

--- src/components/home/ContentCarousel.jsx
+++ src/components/home/ContentCarousel.jsx
@@ -22,7 +22,7 @@ const ContentCarousel = () => {
         className="mySwiper h-80 object-cover rounded-md mb-4"
       >
         <SwiperSlide>
-          <img src="/c.png" alt="Custom Image" className="w-300px h-300px object-cover rounded-md" />
+          <img src="/c.png" alt="Custom Image" className="w-300px h-700px object-cover rounded-md" />
         </SwiperSlide>
       </Swiper>
     </div>
